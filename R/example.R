0308da5825b0394c69de8f7a309534bee57403bf
Gregory Matesi <matesig@math-compute.ucdenver.pvt>
2020-01-10 13:56:33 -0700

    removing example.R

--- R/example.R
+++
@@ -1,4 +0,0 @@
-data <- read.csv("/nfs/storage/math/gross-s2/projects/mixtures/RHiddenAncestries/data/packagedata.csv")
-ref <-data[,c("ref_afr", "ref_eas", "ref_eur", "ref_nam", "ref_sas")]
-obs <- cbind(data$gnomad_afr)
-ancestr(ref,obs)
